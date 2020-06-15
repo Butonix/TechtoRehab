9f9a5f22690b899e6e013ecb27ab56d4e27a3e14
dukesx <afzaalbci@ymail.com>
2020-06-15 15:10:42 +0500

    updated hasura

--- frontend/lib/apolloClient.js
+++ frontend/lib/apolloClient.js
@@ -9,7 +9,7 @@ function createApolloClient() {
   return new ApolloClient({
     ssrMode: typeof window === "undefined",
     link: new HttpLink({
-      uri: "http://localhost:8080/v1/graphql", // Server URL (must be absolute)
+      uri: "https://hasura-server-next.herokuapp.com/v1/graphql", // Server URL (must be absolute)
       credentials: "same-origin", // Additional fetch() options like `credentials` or `headers`
     }),
     cache: new InMemoryCache(),
