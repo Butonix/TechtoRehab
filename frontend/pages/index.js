057caf64814345a4d1beb8f3fe76b3b7cd94db42
dukesx <afzaalbci@ymail.com>
2020-06-18 20:21:21 +0500

    fixed issue with fetch

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -455,7 +455,7 @@ export default function Home() {
   );
 }
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   const apolloClient = initializeApollo();
   await apolloClient.query({
     query: query,
@@ -465,6 +465,5 @@ export const getStaticProps = async () => {
     props: {
       initialApolloState: apolloClient.cache.extract(),
     },
-    unstable_revalidate: 1,
   };
 };
