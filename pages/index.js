f7362adff8e346329f30b15d659ecb9d7f0be804
dukesx <afzaalbci@ymail.com>
2020-07-31 12:46:44 +0500

    see localhost for testing

--- pages/index.js
+++ pages/index.js
@@ -650,7 +650,7 @@ export default function Home(props) {
 export async function getStaticProps(ctx) {
   const apolloClient = initializeApollo();
 
-  return fetch("https://techtorehab/api/getUser")
+  return fetch("http://localhost:3000/api/getUser")
     .then((res) =>
       res.json().then(async (result) => {
         if (result.loggedIn) {
