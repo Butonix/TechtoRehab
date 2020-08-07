da734edcf4e79941a3651f432cb981c1b6119192
dukesx <afzaalbci@ymail.com>
2020-08-07 14:12:41 +0500

    fixed

--- pages/api/recaptcha.js
+++ pages/api/recaptcha.js
@@ -5,7 +5,19 @@ export default (req, res) => {
 
   verify("0x91043d30b19014D57D620FbED548bd5BE92d63E6", token)
     .then((data) => {
-      console.log("success!", data);
+      if (data.success === true) {
+        res.send(
+          JSON.stringify({
+            response: "ok",
+          })
+        );
+      } else {
+        res.send(
+          JSON.stringify({
+            response: "fail",
+          })
+        );
+      }
     })
     .catch(console.error);
 };
