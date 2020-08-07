6e78a08fe539d28c2e8d9d8b90ef29f204d464e0
dukesx <afzaalbci@ymail.com>
2020-08-07 14:33:37 +0500

    data

--- pages/api/recaptcha.js
+++ pages/api/recaptcha.js
@@ -5,6 +5,7 @@ export default (req, res) => {
 
   verify("0x91043d30b19014D57D620FbED548bd5BE92d63E6", token)
     .then((data) => {
+      console.log(data);
       if (data.success === true) {
         res.send(
           JSON.stringify({
