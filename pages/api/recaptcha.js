0439ceeae96047268b20280f249ca1f4f1ed4d3b
dukesx <afzaalbci@ymail.com>
2020-08-07 14:44:08 +0500

    test1

--- pages/api/recaptcha.js
+++ pages/api/recaptcha.js
@@ -2,7 +2,7 @@ const { verify } = require("hcaptcha");
 
 export default (req, res) => {
   const { token } = req.body;
-
+  console.log(token);
   verify("0x91043d30b19014D57D620FbED548bd5BE92d63E6", token)
     .then((data) => {
       console.log(data);
