4146137f3b005d4ccf3fed9aa0b2c0a381ab0a55
dukesx <afzaalbci@ymail.com>
2020-08-07 14:07:14 +0500

    see2

--- pages/api/recaptcha.js
+++ pages/api/recaptcha.js
@@ -1,41 +1,11 @@
-const axios = require("axios");
+const { verify } = require("hcaptcha");
 
 export default (req, res) => {
   const { token } = req.body;
 
-  axios({
-    url: `https://hcaptcha.com/siteverify`,
-
-    data: {
-      secret: "0x91043d30b19014D57D620FbED548bd5BE92d63E6",
-      response: token,
-    },
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  })
-    .then(function (response) {
-      console.log(response);
-      if (response.data.success == true) {
-        res.end(
-          JSON.stringify({
-            response: "ok",
-          })
-        );
-      } else {
-        res.end(
-          JSON.stringify({
-            response: "fail",
-          })
-        );
-      }
+  verify("0x91043d30b19014D57D620FbED548bd5BE92d63E6", token)
+    .then((data) => {
+      console.log("success!", data);
     })
-    .catch(function (error) {
-      console.log(error);
-      res.end(
-        JSON.stringify({
-          response: "fail",
-        })
-      );
-    });
+    .catch(console.error);
 };
